2ee9da268f2afa8ef27a98ef66ec9d259df3e615
Shujinko53 <shujinko53@yandex.ru>
2020-08-28 22:42:40 +0300

    Creating a script

---
+++ source/js/main.js
@@ -0,0 +1,7 @@
+const MOBILE_MAX_WIDTH = 767;
+
+const header = document.querySelector('.header');
+const navigation = document.querySelector('.navigation');
+const MenuButton = document.querySelector('.header__menu-toggle');
+const overlay = document.querySelector('.navigation__overlay');
+
