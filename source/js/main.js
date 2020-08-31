5649382fef714a1814f7e838689019176795b697
Shujinko53 <shujinko53@yandex.ru>
2020-08-31 22:02:11 +0300

    The slider script has been created and JS code has been fixed

--- source/js/main.js
+++ source/js/main.js
@@ -1,73 +1,78 @@
-const MOBILE_MAX_WIDTH = 767;
+'use strict';
 
-const header = document.querySelector('.header');
-const navigation = document.querySelector('.navigation');
-const MenuButton = document.querySelector('.header__menu-toggle');
-const overlay = document.querySelector('.navigation__overlay');
+(function () {
+  const MOBILE_MAX_WIDTH = 767;
 
-function openHeaderMenu() {
+  const header = document.querySelector('.header');
+  const navigation = document.querySelector('.navigation');
+  const MenuButton = document.querySelector('.header__menu-toggle');
+  const overlay = document.querySelector('.navigation__overlay');
 
-  MenuButton.removeEventListener(`click`, openHeaderMenu);
-  MenuButton.addEventListener(`click`, closeHeaderMenu);
-  overlay.addEventListener(`click`, closeHeaderMenu);
+  function openHeaderMenu() {
 
-  document.addEventListener(`keydown`, onEscPress);
+    MenuButton.removeEventListener(`click`, openHeaderMenu);
+    MenuButton.addEventListener(`click`, closeHeaderMenu);
+    overlay.addEventListener(`click`, closeHeaderMenu);
 
-  document.body.classList.add(`no-scroll`)
-  header.classList.add(`header_colored`);
-  navigation.classList.add(`navigation_open`);
-  MenuButton.classList.add(`header__menu-button_active`);
+    document.addEventListener(`keydown`, onEscPress);
 
-  setMenuHeight();
-}
+    document.body.classList.add(`no-scroll`)
+    header.classList.add(`header_colored`);
+    navigation.classList.add(`navigation_open`);
+    MenuButton.classList.add(`header__menu-button_active`);
 
-function closeHeaderMenu() {
-  MenuButton.removeEventListener(`click`, closeHeaderMenu);
-  MenuButton.addEventListener(`click`, openHeaderMenu);
-  overlay.removeEventListener(`click`, closeHeaderMenu);
-
-  document.removeEventListener(`keydown`, onEscPress);
-
-  document.body.classList.remove(`no-scroll`);
-  if (!isScrolled()) {
-    header.classList.remove(`header_colored`);
+    setMenuHeight();
   }
-  navigation.classList.remove(`navigation_open`);
-  MenuButton.classList.remove(`header__menu-button_active`);
-}
 
-function onEscPress(evt) {
-  if (evt.keyCode === 27) {
-    closeHeaderMenu();
-  }
-}
+  function closeHeaderMenu() {
+    MenuButton.removeEventListener(`click`, closeHeaderMenu);
+    MenuButton.addEventListener(`click`, openHeaderMenu);
+    overlay.removeEventListener(`click`, closeHeaderMenu);
 
-function isScrolled() {
-  const pxAmount = 0
-  const scrollTop = document.documentElement.scrollTop;
+    document.removeEventListener(`keydown`, onEscPress);
 
-  return scrollTop > pxAmount;
-}
-
-function setMenuHeight() {
-  navigation.removeAttribute(`style`);
-
-  const deltaHeight = document.body.offsetHeight - header.offsetHeight;
-  const menuHeight = menu.offsetHeight;
-
-  if (deltaHeight < menuHeight) {
-    navigation.setAttribute(`style`, `height: ${deltaHeight}px`);
-  }
-}
-
-if (navigation) {
-  window.addEventListener(`scroll`, function () {
-    if (isScrolled()) {
-      header.classList.add(`header_colored`);
-    } else {
+    document.body.classList.remove(`no-scroll`);
+    if (!isScrolled()) {
       header.classList.remove(`header_colored`);
     }
-  })
+    navigation.classList.remove(`navigation_open`);
+    MenuButton.classList.remove(`header__menu-button_active`);
+  }
 
-  closeHeaderMenu();
-}
+  function onEscPress(evt) {
+    if (evt.keyCode === 27) {
+      closeHeaderMenu();
+    }
+  }
+
+  function isScrolled() {
+    const pxAmount = 0
+    const scrollTop = document.documentElement.scrollTop;
+
+    return scrollTop > pxAmount;
+  }
+
+  function setMenuHeight() {
+    navigation.removeAttribute(`style`);
+
+    const deltaHeight = document.body.offsetHeight - header.offsetHeight;
+    const menuHeight = menu.offsetHeight;
+
+    if (deltaHeight < menuHeight) {
+      navigation.setAttribute(`style`, `height: ${deltaHeight}px`);
+    }
+  }
+
+  if (navigation) {
+    window.addEventListener(`scroll`, function () {
+      if (isScrolled()) {
+        header.classList.add(`header_colored`);
+      } else {
+        header.classList.remove(`header_colored`);
+      }
+    })
+
+    closeHeaderMenu();
+  }
+
+})();
